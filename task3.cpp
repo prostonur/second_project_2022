d1dd2d0f2bbc734b8297cd6e2ef2aa756152c3d3
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2022-10-11 14:27:14 +0500

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -28,7 +28,7 @@ int main()
     /*
     for (int i = 0 ; i <n; i++)
     {
-        for (int j =0; j <= n - i; j++) 
+        for (int j =0; j <n - i; j++) 
             std::cout << " ";
         for (int k = 0; k < 2*(n-i)-1 ; k++) 
             std::cout << "*";
