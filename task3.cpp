7c2d4990c331a4d07d01b2c25feb4562d91dd4d0
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2022-10-11 14:23:52 +0500

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -25,5 +25,16 @@ int main()
 
         std::cout << '\n';
     }
+    /*
+    for (int i = 0 ; i <n; i++)
+    {
+        for (int j =0; j <= n - i; j++) 
+            std::cout << " ";
+        for (int k = 0; k < 2*(n-i)-1 ; k++) 
+            std::cout << "*";
+
+        std::cout << '\n';
+    }
+    */
     return 0;
 }
